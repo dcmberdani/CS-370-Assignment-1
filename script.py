a04c03deecbaa8c6a06cefd53bffa2500d177686
dcmberdani <dcmberdani@gmail.com>
2022-04-26 16:00:08 -0700

    Resized the fiveHashBitArray; Final Commit

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from asyncore import write
 import argparse
 from fileinput import close
 import hashlib
-import time # Just to sate my curiosity
+import time 
 
 #Argument Parser Setup
 parser = argparse.ArgumentParser(description = "Just parses the arguments")
@@ -13,8 +13,8 @@ parser.add_argument("-o5", help="Filepath of output file for passwords tested wi
 args = parser.parse_args()
 
 #Bit Array Setup
-threeHashBitArray = [0] * 16777216 # 16 kb
-fiveHashBitArray = [0] * 16777216 # 16 kb
+threeHashBitArray = [0] * 16777216 # ~16 Mb
+fiveHashBitArray = [0] * 10700000 # 10.7Mb
 
 #Encrypts the plaintext with md5
 #   Salt is added uniformly to all passwords, both trained and tested
@@ -32,7 +32,7 @@ def encrypt(plaintext):
     ciphertext = ciphertext[:6]
     return ciphertext
 
-c
+
 def fullEncrypt(plaintext, hashCount):
     encArray = []
     for x in range(0, hashCount):
@@ -41,15 +41,20 @@ def fullEncrypt(plaintext, hashCount):
     return encArray
 
 
-
-
 #Trains the bit array with the given set of hashes
 #   Does so by converting the hash to an int, then setting that value in the bit array
 #   Thus this needs to be called per bad password
+#   In the case of 5-bit array, also mod the numbers by the size of the bit array
 def trainBitArray(encArray, bitArray):
     numArray = []
     for item in encArray:
         numArray.append( int( item, base = 16 ) )
+
+    #Making sure 5-bit hashes fits in bit array size 
+    if len(numArray) == 5:
+        for i in range(len(numArray)):
+            numArray[i] %= 10700000
+            
 
     for index in numArray:
         bitArray[index] = 1
@@ -61,19 +66,20 @@ def testBitArray(encArray, bitArray):
     for item in encArray:
         numArray.append( int( item, base = 16 ) )
 
-    #print(numArray)
+    #Making sure 5-bit hashes fits in bit array size 
+    if len(numArray) == 5:
+        for i in range(len(numArray)):
+            numArray[i] %= 10700000
+
     counter = 0
 
     for index in numArray:
         if (bitArray[index] == 1):
             counter += 1
-        #print(bitArray[index])
     
     if (counter == len(encArray)):
-        #print("BAD PASSWORD FOUND")
         return 1
     else:
-        #print("GOOD PASSWORD FOUND")
         return 0
     
 
@@ -83,17 +89,24 @@ def main():
         #For every line in the dictionary (i.e. a bad password)
         #   Encrypt the line with MD5 N times with salt
         #   Then, train the bit array by setting the bits at the indices given by the hashes
-        startTrain = time.time()
+        startTrain3 = time.time()
         for line in f:
             currLine = line.rstrip()
             threeEncArray = fullEncrypt(currLine, 3)
             trainBitArray(threeEncArray, threeHashBitArray)
+        endTrain3 = time.time()
 
+    with open(args.d, 'r') as f:
+        startTrain5 = time.time()
+        for line in f:
+            currLine = line.rstrip()
             fiveEncArray = fullEncrypt(currLine, 5)
             trainBitArray(fiveEncArray, fiveHashBitArray)
-        endTrain = time.time()
+        endTrain5 = time.time()
 
-        print("Time it took to train both bit arrays: ~%.2f seconds" % (endTrain - startTrain) )
+        print("Time it took to train the 3-hash bit array: ~%.2f seconds" % (endTrain3 - startTrain3) )
+        print("Time it took to train the 5-hash bit array: ~%.2f seconds" % (endTrain5 - startTrain5) )
+
 
     #For every line in the input file (i.e. a password to test)
     #   Encrypt the line with MD5 N times with the same salt as above
@@ -108,23 +121,23 @@ def main():
 
             threeEncArray = fullEncrypt(currLine, 3)
             if ( testBitArray(threeEncArray, threeHashBitArray) == 1 ):
-                output3.write(currLine + ": maybe (POTENTIAL BAD PASSWORD)\n")
+                output3.write(currLine + ": MAYBE\n")
             else:
-                output3.write(currLine + ": no (GOOD PASSWORD)\n")
-
+                output3.write(currLine + ": NO\n")
 
             fiveEncArray = fullEncrypt(currLine, 5)
             if ( testBitArray(fiveEncArray, fiveHashBitArray) == 1 ):
-                output5.write(currLine + ": maybe (POTENTIAL BAD PASSWORD)\n")
+                output5.write(currLine + ": MAYBE\n")
             else:
-                output5.write(currLine + ": no (GOOD PASSWORD)\n")
+                output5.write(currLine + ": NO\n")
 
         output3.close()
         output5.close()
 
  
 
-    
+#cmd to run
+# .\script.py -d .\dictionary.txt -i .\sample_input.txt -o3 .\output3.txt -o5 .\output5.txt
 
 
 
